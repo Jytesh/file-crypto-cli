7be3ac501aa0c9131892a70a1eacecb6f40721db
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-10-02 15:51:17 +0530

    bump (#1)
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- src/ux.rs
+++ src/ux.rs
@@ -1,29 +1,7 @@
-use dialoguer::{theme::ColorfulTheme, Confirm, Input, Password};
-
-pub fn bool(input: &str) -> Result<bool, std::io::Error> {
-    Confirm::with_theme(&ColorfulTheme::default())
-        .default(true)
-        .with_prompt(input)
-        .interact()
-}
+use dialoguer::{theme::ColorfulTheme, Input};
 
 pub fn input(input: &str) -> Result<String, std::io::Error> {
     Input::<String>::with_theme(&ColorfulTheme::default())
         .with_prompt(input)
         .interact_text()
-}
-
-pub fn input_with_default(input: &str, def: &str) -> Result<String, std::io::Error> {
-    Input::<String>::with_theme(&ColorfulTheme::default())
-        .default(def.to_string())
-        .show_default(true)
-        .with_prompt(input)
-        .interact_text()
-}
-
-pub fn pass(input: &str) -> Result<String, std::io::Error> {
-    Password::with_theme(&ColorfulTheme::default())
-        .with_prompt(input)
-        .allow_empty_password(false)
-        .interact()
 }
